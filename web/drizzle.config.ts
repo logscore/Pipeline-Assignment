924b8012d4c1a9150190aacf6e003fcbdb78eca4
logscore <lsreedercontact@gmail.com>
2026-03-31 13:55:27 -0600

    DB and auth libraries with a db migration

---
+++ web/drizzle.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'drizzle-kit'
+export default defineConfig({
+  schema: './lib/db/schema.ts',
+  out: './lib/db/drizzle',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+})
